7bcb0bcf6dc900f0c7a9ccbcc7bb666758e3355c
kunalpawar04 <pawarkunal3996@gmail.com>
2024-03-02 16:58:54 +0530

    Create model for user

---
+++ models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+// Creating structure of the document for the user
+const userSchema = mongoose.Schema(
+  {
+    userName: {
+      type: String,
+      required: [true, "Please add user name"],
+    },
+    email: {
+      type: String,
+      required: [true, "Please add user email address"],
+      unique: true,
+      // custom validation function for the email field.
+      validate: {
+        validator: async function (value) {
+          const existingUser = await this.constructor.findOne({ email: value });
+          return !existingUser;
+        },
+        //The error message "Email address already taken" will be returned if validation fails.
+        message: "Email address already taken",
+      },
+    },
+    password: {
+      type: String,
+      required: [true, "Please add user password"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
